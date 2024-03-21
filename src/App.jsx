327da62246ca3c355a92fc726104af7effb578c5
Bahar Sevinti <baharsevintii@gmail.com>
2024-03-21 16:09:33 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import Recipes from "./pages/Recipes";
+import AIRecipe from "./pages/AIRecipe";
+import AdminPanel from "./pages/Panel/AdminPanel";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<AdminPanel />} /> */}
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/logIn" element={<LogIn />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/makeRecipe" element={<AIRecipe />} />
+        <Route path="/adminPanel" element={<AdminPanel />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
